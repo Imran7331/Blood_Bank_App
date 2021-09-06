c2a39c03168fbd99ca43aae7379edd65a37bb386
ImranCSS <imran.vtn@gmail.com>
2021-09-06 16:22:36 +0500

    Initial commit

---
+++ app/src/main/java/app/aerocast/bloodbankapp/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package app.aerocast.bloodbankapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import app.aerocast.bloodbankapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
